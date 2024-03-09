7ff9b888a64df6e199a6f33deb456a090efabfb6
nurbolatkz <nurbolat.duisenbek@narxoz.kz>
2024-03-09 21:49:10 +0600

    add deploy version

---
+++ precache-manifest.c70e2b903add45a3a964c4f2d0f88cc7.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956b095aad1af4691600522978403711",
+    "url": "/pet_hospital/index.html"
+  },
+  {
+    "revision": "b800876ce6debf3e20bf",
+    "url": "/pet_hospital/static/css/3.98036eff.chunk.css"
+  },
+  {
+    "revision": "41200563a0efd73635ef",
+    "url": "/pet_hospital/static/css/main.19660136.chunk.css"
+  },
+  {
+    "revision": "30c4235d534742c868ea",
+    "url": "/pet_hospital/static/js/0.5ded4735.chunk.js"
+  },
+  {
+    "revision": "a422c4ca7d36c17c6de56179cdc12598",
+    "url": "/pet_hospital/static/js/0.5ded4735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b800876ce6debf3e20bf",
+    "url": "/pet_hospital/static/js/3.d6597424.chunk.js"
+  },
+  {
+    "revision": "9b55c8c4cd7e6fe84de06c8e874a744a",
+    "url": "/pet_hospital/static/js/3.d6597424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56896156a6024043617d",
+    "url": "/pet_hospital/static/js/4.aba2ad55.chunk.js"
+  },
+  {
+    "revision": "a422c4ca7d36c17c6de56179cdc12598",
+    "url": "/pet_hospital/static/js/4.aba2ad55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6f1e87519d675fcbea",
+    "url": "/pet_hospital/static/js/5.53792701.chunk.js"
+  },
+  {
+    "revision": "a422c4ca7d36c17c6de56179cdc12598",
+    "url": "/pet_hospital/static/js/5.53792701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fdfb5a92f60d576f036",
+    "url": "/pet_hospital/static/js/6.173058e6.chunk.js"
+  },
+  {
+    "revision": "a422c4ca7d36c17c6de56179cdc12598",
+    "url": "/pet_hospital/static/js/6.173058e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd62b1362e7e5b06275",
+    "url": "/pet_hospital/static/js/7.d9410df4.chunk.js"
+  },
+  {
+    "revision": "a422c4ca7d36c17c6de56179cdc12598",
+    "url": "/pet_hospital/static/js/7.d9410df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4c7ede0f2fdcbe1ad4c",
+    "url": "/pet_hospital/static/js/8.f218f6cd.chunk.js"
+  },
+  {
+    "revision": "a422c4ca7d36c17c6de56179cdc12598",
+    "url": "/pet_hospital/static/js/8.f218f6cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50edc6cf438525d7f671",
+    "url": "/pet_hospital/static/js/9.bc955c63.chunk.js"
+  },
+  {
+    "revision": "5013ec5cc423f18a210c3d44baf240ab",
+    "url": "/pet_hospital/static/js/9.bc955c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41200563a0efd73635ef",
+    "url": "/pet_hospital/static/js/main.5345813c.chunk.js"
+  },
+  {
+    "revision": "1e58dba8d95f64895f36",
+    "url": "/pet_hospital/static/js/runtime-main.f2141f2a.js"
+  },
+  {
+    "revision": "4e87d758d6a1bf48a3c737600ac9d60b",
+    "url": "/pet_hospital/static/media/bg.4e87d758.jpg"
+  },
+  {
+    "revision": "81e132bd3cd778455414e72ecf74d29a",
+    "url": "/pet_hospital/static/media/dog.81e132bd.png"
+  },
+  {
+    "revision": "8a35791d78b2bc109b2d7f755fbfe29d",
+    "url": "/pet_hospital/static/media/dog1.8a35791d.jpg"
+  },
+  {
+    "revision": "31bfbfb1d364896145439a0e8b236503",
+    "url": "/pet_hospital/static/media/dog2.31bfbfb1.png"
+  },
+  {
+    "revision": "63959a079dbfc4cbb2cee13356f4fff9",
+    "url": "/pet_hospital/static/media/dog3.63959a07.jpg"
+  },
+  {
+    "revision": "3af8f003470174b88559392a68378b6d",
+    "url": "/pet_hospital/static/media/dog4.3af8f003.jpg"
+  },
+  {
+    "revision": "323cbd3ae1951bd63ad9d06466f892ad",
+    "url": "/pet_hospital/static/media/dog5.323cbd3a.jpg"
+  },
+  {
+    "revision": "1bdc27d479fd8b4cedb647f301595748",
+    "url": "/pet_hospital/static/media/google.1bdc27d4.png"
+  },
+  {
+    "revision": "5f2bbb3de4ef941a4e31a321cf03a0e0",
+    "url": "/pet_hospital/static/media/login-bg.5f2bbb3d.jpg"
+  },
+  {
+    "revision": "967a8874b036add8ed9c2c5c0d8f7002",
+    "url": "/pet_hospital/static/media/services.967a8874.jpg"
+  },
+  {
+    "revision": "210f2624c547503984829af7e59a04c5",
+    "url": "/pet_hospital/static/media/services1.210f2624.jpg"
+  },
+  {
+    "revision": "39d370dbe64935d155e2dcf8a6153ca0",
+    "url": "/pet_hospital/static/media/services3.39d370db.jpg"
+  },
+  {
+    "revision": "6f3d5a6f747e8aefdab89bc061e5d2d0",
+    "url": "/pet_hospital/static/media/team1.6f3d5a6f.jpg"
+  },
+  {
+    "revision": "6a265de6adb87aa7da9802226d59a17b",
+    "url": "/pet_hospital/static/media/team2.6a265de6.jpg"
+  },
+  {
+    "revision": "6c4996c3904813715ea8b21affe2b9fa",
+    "url": "/pet_hospital/static/media/team3.6c4996c3.jpg"
+  }
+]);
